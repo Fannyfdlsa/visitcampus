6fa97790fe4e28ddabb575964dc89f96ccaafd18
Fanny Fadlilatunnisa <fannyfadlila025@gmail.com>
2023-12-08 16:00:40 +0700

    user commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/auth', authRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ models/user.js
@@ -0,0 +1,30 @@
+const { Sequelize } = require('sequelize');
+const config = require('../config/db');
+
+const sequelize = new Sequelize(
+  config.development.database,
+  config.development.username,
+  config.development.password,
+  {
+    host: config.development.host,
+    dialect: config.development.dialect,
+  }
+);
+
+const User = sequelize.define('User', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = { User, sequelize };
